b35422e83b90f0258cb85a680e138632bd0dfb90
Hi5000Hi <54914761+Hi5000Hi@users.noreply.github.com>
2019-10-21 14:37:19 -0600

    Firebase and Travis Set up

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyD22ffuYjuvTPm1u33Y64qbmgmE-fi8m9o",
-  authDomain: "authentication2-a2c4b.firebaseapp.com",
-  databaseURL: "https://authentication2-a2c4b.firebaseio.com",
-  projectId: "authentication2-a2c4b",
-  storageBucket: "",
-  messagingSenderId: "365655359740",
-  appId: "1:365655359740:web:b34c6c42488fcdb0cdd779"
+  apiKey: "AIzaSyB9Uuehl066Lmt3z0G0NtDuDTdNfA6A2DQ",
+  authDomain: "album-3c929.firebaseapp.com",
+  databaseURL: "https://album-3c929.firebaseio.com",
+  projectId: "album-3c929",
+  storageBucket: "album-3c929.appspot.com",
+  messagingSenderId: "414304714523",
+  appId: "1:414304714523:web:927486529e68a0f79c9dbe"
 };
 
 firebase.initializeApp(config);
