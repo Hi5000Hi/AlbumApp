b62354a46866bc39ad3efbf4e837ace23fdb10bd
Hi5000Hi <54914761+Hi5000Hi@users.noreply.github.com>
2019-12-02 13:51:25 -0700

    Firebase/Travis settings

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyD22ffuYjuvTPm1u33Y64qbmgmE-fi8m9o",
-  authDomain: "authentication2-a2c4b.firebaseapp.com",
-  databaseURL: "https://authentication2-a2c4b.firebaseio.com",
-  projectId: "authentication2-a2c4b",
-  storageBucket: "",
-  messagingSenderId: "365655359740",
-  appId: "1:365655359740:web:b34c6c42488fcdb0cdd779"
+  apiKey: "AIzaSyDd7eC4Xl1PmNRLjdfvE0fy-Rl1q6hpYwU",
+  authDomain: "edtech-c6501.firebaseapp.com",
+  databaseURL: "https://edtech-c6501.firebaseio.com",
+  projectId: "edtech-c6501",
+  storageBucket: "edtech-c6501.appspot.com",
+  messagingSenderId: "923465652977",
+  appId: "1:923465652977:web:0d4e8928a4fc9d968fe750",
+  measurementId: "G-L0MWQ87R4T"
 };
 
 firebase.initializeApp(config);
